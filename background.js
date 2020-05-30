41934ea1316df4fa9a172799794b8870f5685dab
nasu <tatu.nasu@gmail.com>
2020-05-31 01:18:40 +0900

    feat: add scripts

---
+++ background.js
@@ -0,0 +1,35 @@
+'use strict'
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    //console.log(message)
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+        fetch(
+            'https://texttospeech.googleapis.com/v1beta1/text:synthesize',
+            {
+                method: 'POST',
+                asnyc: true,
+                headers: {
+                    Authorization: 'Bearer ' + token,
+                    'Content-Type': 'application/json'
+                },
+                'contentType': 'json',
+                body: JSON.stringify({
+                    input: {
+                        text: message.text,
+                    },
+                    voice: {
+                        languageCode: 'zh-CN',
+                        ssmlGender: 'NEUTRAL',
+                    },
+                    audioConfig: {
+                        audioEncoding: 'MP3',
+                    },
+                })
+            }
+        )
+        .then((res) => res.json())
+        .then((data) => {
+            sendResponse({data: data.audioContent})
+        })
+    })
+    return true
+})
